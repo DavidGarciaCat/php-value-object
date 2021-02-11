5c8ab0e812a707c119e6c45888b54a06a156d13b
David Garcia <david.garcia@deralia.com>
2021-02-11 07:50:02 +0000

    Customise input arguments for this library's Exception

--- src/Exception/InvalidValueException.php
+++ src/Exception/InvalidValueException.php
@@ -5,7 +5,12 @@ declare(strict_types=1);
 namespace DavidGarcia\ValueObject\Exception;
 
 use Exception;
+use Throwable;
 
 class InvalidValueException extends Exception
 {
+    public function __construct($message = "", Throwable $previous = null)
+    {
+        parent::__construct($message, 0, $previous);
+    }
 }
